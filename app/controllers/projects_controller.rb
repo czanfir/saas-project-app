18d157cdf4c80a8b2fa503c5529ee965cad80cff
Cristi Zanfir <zanfircristian92@gmail.com>
2018-02-04 14:02:22 +0000

    Fix another set of bugs

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -26,7 +26,7 @@ class ProjectsController < ApplicationController
   # POST /projects.json
   def create
     @project = Project.new(project_params)
-    @project.users << curent_user
+    @project.users << current_user
     respond_to do |format|
       if @project.save
         format.html { redirect_to root_url, notice: 'Project was successfully created.' }
@@ -58,17 +58,18 @@ class ProjectsController < ApplicationController
   end
   
   def users
-    @project_users = (@project.users + (User.where(tenant_id: @tenant.id, is_admin: true))) - [curent_user]
-    @other_users = @tenant.users.where(is_admin: false) - (@project_users + [curent_user])
+    @project_users = (@project.users + (User.where(tenant_id: @tenant.id, is_admin: true))) - [current_user]
+    @other_users = @tenant.users.where(is_admin: false) - (@project_users + [current_user])
   end
   
   def add_user
-    @project_user = UserProject.new(user_id: param[:user_id], project_id: @project.id)
+    @project_user = UserProject.new(user_id: params[:user_id], project_id: @project.id)
     respond_to do |format|
       if @project_user.save
         format.html { redirect_to users_tenant_project_url(id: @project.id, tenant_id: @project.tenant_id), notice: "User was successfully added to project"}
       else
         format.html { redirect_to users_tenant_project_url(id: @project.id, tenant_id: @project.tenant_id), error: "User was not added to project" }
+      end
     end
   end
 
